cef473a39c333d0d7a31d474e332f04264783da2
Rony Agung Rahmanto <lantip@gmail.com>
2014-01-23 02:01:50 +0700

    fixing enqueue script in wp plugins

--- wp-badudujs/badudu-js.php
+++ wp-badudujs/badudu-js.php
@@ -9,6 +9,8 @@ Author URI: http://lantip.net
 License: ABRMS License
 */
 
+add_action( 'init', 'badudu_css_script' );
+add_action( 'wp_enqueue_scripts', 'badudu_js_script' );
 function badudu_js_script() {
     //wp_enqueue_script('jquery');
 
@@ -21,5 +23,3 @@ function badudu_css_script(){
     wp_register_style( 'badudujs-css', get_bloginfo('wpurl').'/wp-content/plugins/wp-badudujs' . '/badudujs.css',array(),'', 'screen' );
     wp_enqueue_style( 'badudujs-css' );
 }
-add_action( 'init', 'badudu_css_script' );
-add_action( 'init', 'badudu_js_script' );
